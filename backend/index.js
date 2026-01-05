47358c5f633325145e843d864fb38ab190984875
sanskargangwar <145826905+sanskargangwar@users.noreply.github.com>
2026-01-06 00:36:42 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://five-onecart-frontend-6hc8.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
